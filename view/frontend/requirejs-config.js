b19c3542034fa71098f1f1d79c8bc8b439eb81cf
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-04 22:45:06 +0200

    #6 - Create RequireJS aliases with map and paths

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,3 +1,10 @@
 var config = {
-    
+    "map": {
+        "*": {
+            "fadeInElement": "Biglidio_JsFun/js/fade-in-element"
+        }
+    },
+    "path": {
+        "vue": "https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"
+    }
 }
