8d5f39d521d8ff9905debea00c36d60c888416e3
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-10-04 23:47:22 +0200

    #8 - Imperatively initialize a JavaScript component

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -4,7 +4,7 @@ var config = {
             "fadeInElement": "Biglidio_JsFun/js/fade-in-element"
         }
     },
-    "path": {
-        "vue": "https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"
+    "paths": {
+        "vue": "https://cdn.jsdelivr.net/npm/vue@2/dist/vue"
     }
 }
